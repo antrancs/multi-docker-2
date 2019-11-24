ac88f6e51e41396b6b04fe5273490b78b6078431
An Tran <an.tran@canaldigital.com>
2019-11-24 15:58:35 +0100

    Update client

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import OtherPage from "./OtherPage";
-import Fib from "./Fib";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <Link to="/">Home</Link>
+          <Link to="/">Home Page</Link>
           <Link to="/otherpage">Other page</Link>
         </header>
 
